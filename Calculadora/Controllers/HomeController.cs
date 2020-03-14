8307e1898f321683f41d735956c31d9fb7fe868d
Toby Selway <tobyselway@outlook.com>
2020-03-14 15:10:20 +0000

    Calculadora Implementada

---
+++ Calculadora/Controllers/HomeController.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Calculadora.Models;
+using Microsoft.AspNetCore.Http;
+
+namespace Calculadora.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            HttpContext.Session.SetString("first_num", "");
+            HttpContext.Session.SetString("second_num", "");
+            HttpContext.Session.SetString("op_selected", "");
+            HttpContext.Session.SetString("last_expression", "");
+
+            String first = HttpContext.Session.GetString("first_num");
+            ViewBag.display = (first=="" ? "0" : first) + " " +
+                              HttpContext.Session.GetString("op_selected") + " " +
+                              HttpContext.Session.GetString("second_num");
+            
+            ViewBag.lastDisplay = HttpContext.Session.GetString("last_expression");
+            
+            return View();
+        }
+        
+        [HttpPost]
+        public IActionResult Index(String type, String value)
+        {
+            switch (type)
+            {
+                case "digit":
+                    if (HttpContext.Session.GetString("op_selected") != "")
+                    {
+                        HttpContext.Session.SetString("second_num", HttpContext.Session.GetString("second_num") + value);
+                    }
+                    else
+                    {
+                        HttpContext.Session.SetString("first_num", HttpContext.Session.GetString("first_num") + value);
+                    }
+                    break;
+                case "operation":
+                    if (HttpContext.Session.GetString("first_num") == "")
+                        HttpContext.Session.SetString("first_num", "0");
+                    HttpContext.Session.SetString("op_selected", value);
+                    break;
+                case "function":
+                    switch (value)
+                    {
+                        case "separator":
+                            if (HttpContext.Session.GetString("op_selected") != "")
+                            {
+                                String secondNum = HttpContext.Session.GetString("second_num");
+                                if(!secondNum.Contains("."))
+                                    HttpContext.Session.SetString("second_num", (secondNum == "" ? "0" : secondNum) + ".");
+                            }
+                            else
+                            {
+                                String firstNum = HttpContext.Session.GetString("first_num");
+                                if(!firstNum.Contains("."))
+                                    HttpContext.Session.SetString("first_num", (firstNum == "" ? "0" : firstNum) + ".");
+                            }
+                            break;
+                        case "negative":
+                            if (HttpContext.Session.GetString("op_selected") != "")
+                            {
+                                double secondVal = Convert.ToDouble(HttpContext.Session.GetString("second_num") == "" ? "0" : HttpContext.Session.GetString("second_num"));
+                                secondVal *= -1;
+                                HttpContext.Session.SetString("second_num", "" + secondVal);
+                            }
+                            else
+                            {
+                                double firstVal = Convert.ToDouble(HttpContext.Session.GetString("first_num") == "" ? "0" : HttpContext.Session.GetString("first_num"));
+                                firstVal *= -1;
+                                HttpContext.Session.SetString("first_num", "" + firstVal);
+                            }
+                            break;
+                        case "clear":
+                            return Index();
+                        case "equals":
+                            double first = Convert.ToDouble(HttpContext.Session.GetString("first_num"));
+                            double second = Convert.ToDouble(HttpContext.Session.GetString("second_num") == "" ? "0" : HttpContext.Session.GetString("second_num"));
+                            double result = 0;
+                            switch (HttpContext.Session.GetString("op_selected"))
+                            {
+                                case ":":
+                                    result = first / second;
+                                    break;
+                                case "x":
+                                    result = first * second;
+                                    break;
+                                case "-":
+                                    result = first - second;
+                                    break;
+                                case "+":
+                                    result = first + second;
+                                    break;
+                                default:
+                                    result = first;
+                                    break;
+                            }
+                            HttpContext.Session.SetString("last_expression", HttpContext.Session.GetString("first_num") + " " +
+                                                                             HttpContext.Session.GetString("op_selected") + " " +
+                                                                             HttpContext.Session.GetString("second_num") + " = ");
+                            HttpContext.Session.SetString("first_num", "" + result);
+                            HttpContext.Session.SetString("second_num", "");
+                            HttpContext.Session.SetString("op_selected", "");
+                            break;
+                    }
+                    break;
+            }
+
+            ViewBag.display = HttpContext.Session.GetString("first_num") + " " +
+                              HttpContext.Session.GetString("op_selected") + " " +
+                              HttpContext.Session.GetString("second_num");
+            
+            ViewBag.lastDisplay = HttpContext.Session.GetString("last_expression");
+            
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel {RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier});
+        }
+    }
+}
